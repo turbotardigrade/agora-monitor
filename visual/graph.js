442805e794c9a4d838f464d2aceb1f5f0b2a6643
Long Hoang <long@mindworker.de>
2017-04-17 15:15:35 +0800

    Create helpers for graph visual to simply code usage

--- visual/graph.js
+++ visual/graph.js
@@ -1,197 +1,150 @@
-const svg = d3.select('svg')
+const svg = d3.select('svg');
 
-  const data = {
-    nodes: [{
-      id: 'a',
-      radius: 20,
-      fill: 'red',
-      x: 50,
-      y: 100
-    }, {
-      id: 'b',
-      radius: 20,
-      fill: 'green',
-      x: 200,
-      y: 100
-    }, {
-      id: 'c',
-      radius: 20,
-      fill: 'green',
-      x: 350,
-      y: 100
-    }, {
-      id: 'd',
-      radius: 20,
-      fill: 'green',
-      x: 500,
-      y: 200
-    }, {
-      id: 'e',
-      radius: 20,
-      fill: 'green',
-      x: 650,
-      y: 200
-    }, {
-      id: 'f',
-      radius: 20,
-      fill: 'green',
-      x: 50,
-      y: 300
-    }, {
-      id: 'g',
-      radius: 20,
-      fill: 'green',
-      x: 200,
-      y: 300
-    }, {
-      id: 'h',
-      radius: 20,
-      fill: 'green',
-      x: 350,
-      y: 300
-    }],
-    links: [{
-      source: 'a',
-      target: 'b',
-      stroke: 'red'
-    }, {
-      source: 'a',
-      target: 'g',
-      stroke: 'red'
-    }, {
-      source: 'a',
-      target: 'h',
-      stroke: 'red'
-    }, {
-      source: 'f',
-      target: 'g',
-      stroke: 'green'
-    }, {
-      source: 'g',
-      target: 'h',
-      stroke: 'green'
-    }, {
-      source: 'g',
-      target: 'c',
-      stroke: 'green'
-    }, {
-      source: 'b',
-      target: 'c',
-      stroke: 'green'
-    }, {
-      source: 'c',
-      target: 'h',
-      stroke: 'green'
-    }, {
-      source: 'c',
-      target: 'd',
-      stroke: 'green'
-    }, {
-      source: 'h',
-      target: 'd',
-      stroke: 'green'
-    }, {
-      source: 'd',
-      target: 'e',
-      stroke: 'green'
-    }]
-  }
+function createNode(id, x, y, col) {
+  return { id: id, radius: 20, fill: col, x, y };
+}
 
-const gNodes = svg.append('g')
-		  .attr('class', 'nodes')
-  const gLinks = svg.append('g')
-		    .attr('class', 'links')
+function createLink(source, target, stroke) {
+  return { source, target, stroke };
+}
 
-  // Takes an object representing the state of the network and draws it
-  // Warning, possible mindfuck
-  // Helpful links:
-  // https://bost.ocks.org/mike/selection/
-  // https://bost.ocks.org/mike/join/
-  // https://bl.ocks.org/mbostock/3808218
-  function draw(data) {
-    // DATA JOIN
-    // Join new data with old elements, if any.
+const data = {
+  nodes:[
+    createNode('a', 50, 100, 'red'),
+    createNode('b', 200, 100, 'green'),
+    createNode('c', 350, 100, 'green'),
+    createNode('d', 500, 200, 'green'),
+    createNode('e', 650, 200, 'green'),
+    createNode('f', 50, 300, 'green'),
+    createNode('g', 200, 300, 'green'),
+    createNode('h', 350, 300, 'green')
+  ],
+  links: [
+    createLink('a', 'b', 'red'),
+    createLink('a', 'g', 'red'),
+    createLink('a', 'h', 'red'),
+    createLink('f', 'g', 'green'),
+    createLink('g', 'h', 'green'),
+    createLink('g', 'c', 'green'),
+    createLink('b', 'c', 'green'),
+    createLink('c', 'h', 'green'),
+    createLink('c', 'd', 'green'),
+    createLink('h', 'd', 'green'),
+    createLink('d', 'e', 'green')
+  ]
+};
 
-    const node = gNodes
+const gNodes = svg.append('g').attr('class', 'nodes');
+const gLinks = svg.append('g').attr('class', 'links');
+
+// Takes an object representing the state of the network and draws it
+// Warning, possible mindfuck
+// Helpful links:
+// https://bost.ocks.org/mike/selection/
+// https://bost.ocks.org/mike/join/
+// https://bl.ocks.org/mbostock/3808218
+function draw(data) {
+  // DATA JOIN
+  // Join new data with old elements, if any.
+
+  const node = gNodes
     .selectAll('g')
     .data(data.nodes)
-      const link = gLinks
+    const link = gLinks
     .selectAll('line')
-    .data(data.links)
+    .data(data.links);
 
-      // ENTER
-      // Create new elements as needed.
-      //
-      // ENTER + UPDATE
-      // After merging the entered elements with the update selection,
-      // apply operations to both.
+  // ENTER
+  // Create new elements as needed.
+  //
+  // ENTER + UPDATE
+  // After merging the entered elements with the update selection,
+  // apply operations to both.
 
-      const nodeEnter = node.enter().append('g')
+  const nodeEnter = node.enter().append('g');
 
-      nodeEnter
-			    .append('circle')
-      nodeEnter
-			    .append('text')
-      nodeEnter
-			    .attr('transform', d => `translate(${d.x},${d.y})`)
+  nodeEnter.append('circle');
+  nodeEnter.append('text');
+  nodeEnter.attr('transform', d => `translate(${d.x},${d.y})`);
 
-      const nodeUpdate = nodeEnter.merge(node)   
+  const nodeUpdate = nodeEnter.merge(node);
 
-      nodeUpdate.select('circle')
-				  .attr('r', d => d.radius)
-				  .style('fill', d => d.fill)
-      nodeUpdate.select('text')
-				  .attr('text-anchor', 'middle')
-				  .attr('dy', '0.35em') // http://stackoverflow.com/a/8684888/4131237
-				  .text(d => d.id)
+  nodeUpdate.select('circle')
+	    .attr('r', d => d.radius)
+	    .style('fill', d => d.fill);
+  nodeUpdate.select('text')
+	    .attr('text-anchor', 'middle')
+	    .attr('dy', '0.35em') // http://stackoverflow.com/a/8684888/4131237
+	    .text(d => d.id);
 
-      const linkEnter = link.enter().append('line')
+  const linkEnter = link.enter().append('line');
 
-      // HACK
-      // I had to use <g> elements as a container for <circle> and <text>
-      // But <g> elements don't have coordinate attributes
-      // So I tried getting their rendered coordinates using getBoundingClientRect()
-      // However even that doesn't have x and y attributes
-      // but they have left and top which seem to be the same thing according to
-      // https://developer.mozilla.org/en-US/docs/Mozilla/Tech/XPCOM/Reference/Interface/nsIDOMClientRect
-      // This is probably why the link drawing is a bit off
-      // TODO figure out a better solution
-      linkEnter
-			    .attr('x1', d => gNodes.selectAll('g')
-						   .filter(g => g.id === d.source)
-						   .node()
-						   .getBoundingClientRect().left)
-			    .attr('y1', d => gNodes.selectAll('g')
-						   .filter(g => g.id === d.source)
-						   .node()
-						   .getBoundingClientRect().top)
-			    .attr('x2', d => gNodes.selectAll('g')
-						   .filter(g => g.id === d.target)
-						   .node()
-						   .getBoundingClientRect().left)
-			    .attr('y2', d => gNodes.selectAll('g')
-						   .filter(g => g.id === d.target)
-						   .node()
-						   .getBoundingClientRect().top)
+  // HACK
+  // I had to use <g> elements as a container for <circle> and <text>
+  // But <g> elements don't have coordinate attributes
+  // So I tried getting their rendered coordinates using getBoundingClientRect()
+  // However even that doesn't have x and y attributes
+  // but they have left and top which seem to be the same thing according to
+  // https://developer.mozilla.org/en-US/docs/Mozilla/Tech/XPCOM/Reference/Interface/nsIDOMClientRect
+  // This is probably why the link drawing is a bit off
+  // TODO figure out a better solution
+  linkEnter.attr('x1', d => gNodes.selectAll('g')
+				  .filter(g => g.id === d.source)
+				  .node()
+				  .getBoundingClientRect().left)
+		      .attr('y1', d => gNodes.selectAll('g')
+					     .filter(g => g.id === d.source)
+					     .node()
+					     .getBoundingClientRect().top)
+		      .attr('x2', d => gNodes.selectAll('g')
+					     .filter(g => g.id === d.target)
+					     .node()
+					     .getBoundingClientRect().left)
+		      .attr('y2', d => gNodes.selectAll('g')
+					     .filter(g => g.id === d.target)
+					     .node()
+					     .getBoundingClientRect().top);
 
-      const linkUpdate = linkEnter.merge(link)
+  const linkUpdate = linkEnter.merge(link);
 
-      linkUpdate
-				  .style('stroke', d => d.stroke)
+  linkUpdate.style('stroke', d => d.stroke);
 
-      // EXIT
-      // Remove old elements as needed.
-      node.exit().remove()
-      link.exit().remove()
+  // EXIT
+  // Remove old elements as needed.
+  node.exit().remove();
+  link.exit().remove();
+};
+
+draw(data);
+
+function getNode(id) {
+  for (i=0; i<data.nodes.length; ++i) {
+    if (data.nodes[i].id === id) {
+      return data.nodes[i]
+    }
   }
 
-draw(data)
+  console.log("Error: ID "+id+" not found");
+}
 
-  // Example of how to redraw with new data
-  // Just call draw() again with new data
-  setTimeout(function() {
-    data.nodes[0].fill = 'blue'
-    data.links[2].stroke = 'purple'
-    
-    draw(data)
-  }, 2000)
+function getLink(id1, id2) {
+  for (i=0; i<data.links.length; ++i) {
+    var link = data.links[i];
+    var isLink1 = link.source === id1 && link.target === id2;
+    var isLink2 = link.source === id2 && link.target === id1;
+    if (isLink1 || isLink2) {
+      return link;
+    }
+  }
+
+  conosle.log("Error: Link between "+id1+" and "+id2+" not found");
+}
+
+// Example of how to redraw with new data
+// Just call draw() again with new data
+setTimeout(function() {
+  getNode('b').fill = 'black';
+  getLink('a', 'b').stroke = 'black';
+
+  draw(data);
+}, 2000);
